5b8dd6c1ecaa838a76d91deba92664ede89e2ac9
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2019-11-01 15:59:13 -0500

    Creando screen de HomePage

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        body: SafeArea(
+      child: Text('Hola mundo!!!!!!!!!!!!!!!!!!!!!!!!!'),
+    ));
+  }
+}
