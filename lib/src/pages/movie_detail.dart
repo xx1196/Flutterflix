57a616f0c0f0d5516090a4234b6127b413d3b6ae
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2019-11-02 15:15:52 -0500

    Agregando imagen de la pelicula y descripcion

--- lib/src/pages/movie_detail.dart
+++ lib/src/pages/movie_detail.dart
@@ -8,14 +8,23 @@ class MovieDetail extends StatelessWidget {
 
     return Scaffold(
         body: CustomScrollView(
-      slivers: <Widget>[_createAppBar(movie)],
+      slivers: <Widget>[
+        _createAppBar(movie),
+        SliverList(
+          delegate: SliverChildListDelegate([
+            SizedBox(height: 10.0),
+            _posterTitle(context, movie),
+            _descriptionMovie(movie),
+          ]),
+        )
+      ],
     ));
   }
 
   Widget _createAppBar(Movie movie) {
     return SliverAppBar(
       elevation: 2.0,
-      backgroundColor: Colors.indigoAccent,
+      backgroundColor: Colors.deepPurpleAccent,
       expandedHeight: 200.0,
       floating: false,
       pinned: true,
@@ -31,6 +40,60 @@ class MovieDetail extends StatelessWidget {
           fadeInDuration: Duration(milliseconds: 150),
           fit: BoxFit.cover,
         ),
+      ),
+    );
+  }
+
+  Widget _posterTitle(BuildContext context, Movie movie) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image(
+              image: NetworkImage(movie.getPosterImg()),
+              height: 150.0,
+            ),
+          ),
+          SizedBox(width: 20.0),
+          Flexible(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                movie.title,
+                style: Theme.of(context).textTheme.title,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Text(
+                movie.originalTitle,
+                style: Theme.of(context).textTheme.subhead,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Row(
+                children: <Widget>[
+                  Icon(Icons.star_border),
+                  Text(
+                    movie.voteAverage.toString(),
+                    style: Theme.of(context).textTheme.subhead,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              )
+            ],
+          )),
+        ],
+      ),
+    );
+  }
+
+  Widget _descriptionMovie(Movie movie) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+      child: Text(
+        movie.overview,
+        textAlign: TextAlign.justify,
       ),
     );
   }
