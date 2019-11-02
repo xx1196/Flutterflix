9d18a40f5509b873f3c59f6adc1d6b3cf9af3727
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2019-11-02 11:49:08 -0500

    Creando un Stream de películas

--- lib/src/Providers/MovieProvider.dart
+++ lib/src/Providers/MovieProvider.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutterflix/src/Models/Movie.dart';
@@ -6,9 +7,23 @@ import 'package:http/http.dart' as http;
 class MoviesProvider {
   String _apiKey = 'd90f883e396197afa8e96a520bddab08';
   String _url = 'api.themoviedb.org';
-  String _language = 'es-ES';
+  String _language = 'es-MX';
+  String _apiVersion = '3';
+  int _popularsPage = 1;
+  List<Movie> _populars = List();
+  final _popularsStreamController = StreamController<List<Movie>>.broadcast();
 
-  Future<List<Movie>> processResponse(Uri url) async {
+  //Agregar datos al stream
+  Function(List<Movie>) get popularsSink => _popularsStreamController.sink.add;
+
+  //Get para obtener los datos del stream
+  Stream<List<Movie>> get popularsStream => _popularsStreamController.stream;
+
+  void disposeStreams() {
+    _popularsStreamController?.close();
+  }
+
+  Future<List<Movie>> _processResponse(Uri url) async {
     final resp = await http.get(url);
     final decodedData = json.decode(resp.body);
     final movies = Movies.fromJsonList(decodedData['results']);
@@ -16,20 +31,27 @@ class MoviesProvider {
   }
 
   Future<List<Movie>> getInCinemas() async {
-    final url = Uri.https(_url, '3/movie/now_playing', {
+    final url = Uri.https(_url, '$_apiVersion/movie/now_playing', {
       'api_key': _apiKey,
       'language': _language,
     });
 
-    return processResponse(url);
+    return await _processResponse(url);
   }
 
   Future<List<Movie>> getPopulars() async {
-    final url = Uri.https(_url, '3/movie/popular', {
+    final url = Uri.https(_url, '$_apiVersion/movie/popular', {
       'api_key': _apiKey,
       'language': _language,
+      'page': _popularsPage.toString(),
     });
-    
-    return processResponse(url);
+    _popularsPage++;
+
+    final resp = await _processResponse(url);
+
+    _populars.addAll(resp);
+    popularsSink(_populars);
+
+    return resp;
   }
 }
